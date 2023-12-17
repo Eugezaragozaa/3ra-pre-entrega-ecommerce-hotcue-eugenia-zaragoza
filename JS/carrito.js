401aa3718bc6fa10f91393d8016bd2c2671b735a
Eugenia Zaragoza <eugeniazaragoza.lab@gmail.com>
2023-12-16 21:45:11 -0300

    ecommerce modificado utilizando todas las tecnologias que se pidieron para la entrega final

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,34 +1,30 @@
 let productosEnCarrito = localStorage.getItem("productos-en-carrito");
 productosEnCarrito = JSON.parse(productosEnCarrito);
+const contenedorCarritoVacio = document.querySelector ("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector ("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector ("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector ("#carrito-comprado");
+let botonesEliminar = document.querySelector(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector ("#carrito-acciones-vaciar");
+const botonComprar = document.querySelector ("#carrito-acciones-comprar");
 
-const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
-const contenedorCarritoProductos = document.querySelector("#carrito-productos");
-const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
-let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
-const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
-const contenedorTotal = document.querySelector("#total");
-const botonComprar = document.querySelector("#carrito-acciones-comprar");
-
-
-function cargarProductosCarrito() {
-    if (productosEnCarrito && productosEnCarrito.length > 0) {
-
-        contenedorCarritoVacio.classList.add("disabled");
+function cargarProductosCarrito(){
+    if (productosEnCarrito && productosEnCarrito.length > 0) { 
+        contenedorCarritoVacio.classList.add ("disabled");
         contenedorCarritoProductos.classList.remove("disabled");
         contenedorCarritoAcciones.classList.remove("disabled");
         contenedorCarritoComprado.classList.add("disabled");
-    
+
         contenedorCarritoProductos.innerHTML = "";
-    
+
         productosEnCarrito.forEach(producto => {
-    
-            const div = document.createElement("div");
+
+            const div = document.createElement ("div");
             div.classList.add("carrito-producto");
-            div.innerHTML = `
+            div.innerHTML=`
                 <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
                 <div class="carrito-producto-titulo">
-                    <small>Título</small>
+                    <small>Titulo</small>
                     <h3>${producto.titulo}</h3>
                 </div>
                 <div class="carrito-producto-cantidad">
@@ -36,32 +32,35 @@ function cargarProductosCarrito() {
                     <p>${producto.cantidad}</p>
                 </div>
                 <div class="carrito-producto-precio">
-                    <small>Precio</small>
-                    <p>$${producto.precio}</p>
+                    <small>precio</small>
+                    <p>${producto.precio}</p>
                 </div>
                 <div class="carrito-producto-subtotal">
                     <small>Subtotal</small>
-                    <p>$${producto.precio * producto.cantidad}</p>
+                    <p>${producto.precio * producto.cantidad}</p>
                 </div>
-                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+                <button class="carrito-producto-eliminar"><i class="bi bi-trash"></i></button>
             `;
-    
-            contenedorCarritoProductos.append(div);
-        })
-    
-    actualizarBotonesEliminar();
-    actualizarTotal();
-	
+
+                contenedorCarritoProductos.append(div);
+
+            })
+
+
     } else {
-        contenedorCarritoVacio.classList.remove("disabled");
+
+        contenedorCarritoVacio.classList.remove ("disabled");
         contenedorCarritoProductos.classList.add("disabled");
         contenedorCarritoAcciones.classList.add("disabled");
         contenedorCarritoComprado.classList.add("disabled");
     }
 
+    actualizarBotonesEliminar();
+    actualizarTotal();
 }
 
 cargarProductosCarrito();
+
 
 function actualizarBotonesEliminar() {
     botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
@@ -72,6 +71,7 @@ function actualizarBotonesEliminar() {
 }
 
 function eliminarDelCarrito(e) {
+
     Toastify({
         text: "Producto eliminado",
         duration: 3000,
@@ -80,50 +80,45 @@ function eliminarDelCarrito(e) {
         position: "right", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-            background: "linear-gradient(to right, #4b33a8, #785ce9)",
+            background: "linear-gradient(to right, rgba(188, 81, 237, 0.65), rgb(0, 255, 13))",
             borderRadius: "2rem",
-            textTransform: "uppercase",
-            fontSize: ".75rem"
         },
-        offset: {
-            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
-            },
         onClick: function(){} // Callback after click
         }).showToast();
 
     const idBoton = e.currentTarget.id;
     const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-    
-    productosEnCarrito.splice(index, 1);
+
+    productosEnCarrito.splice(index,1);
     cargarProductosCarrito();
 
+
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-
 }
-
 botonVaciar.addEventListener("click", vaciarCarrito);
 function vaciarCarrito() {
 
     Swal.fire({
-        title: '¿Estás seguro?',
+        title: "¿estas seguro?",
         icon: 'question',
-        html: `Se van a borrar ${productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0)} productos.`,
+        html: `se borraran todos los productos`,
         showCancelButton: true,
         focusConfirm: false,
-        confirmButtonText: 'Sí',
-        cancelButtonText: 'No'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            productosEnCarrito.length = 0;
-            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-            cargarProductosCarrito();
-        }
+        confirmButtonText: `si`,
+        cancelButtonText: `permanecer`,
+        cancelButtonAriaLabel: "Thumbs down"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                productosEnCarrito.length = 0;
+                localStorage.setItem ("productos-en-carrito", JSON.stringify(productosEnCarrito));
+                cargarProductosCarrito();
+
+            }
         })
+
 }
 
-
-function actualizarTotal() {
+function actualizarTotal(){
     const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
     total.innerText = `$${totalCalculado}`;
 }
@@ -132,11 +127,10 @@ botonComprar.addEventListener("click", comprarCarrito);
 function comprarCarrito() {
 
     productosEnCarrito.length = 0;
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-    
-    contenedorCarritoVacio.classList.add("disabled");
+    localStorage.setItem ("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+    contenedorCarritoVacio.classList.add ("disabled");
     contenedorCarritoProductos.classList.add("disabled");
     contenedorCarritoAcciones.classList.add("disabled");
     contenedorCarritoComprado.classList.remove("disabled");
-
 }
